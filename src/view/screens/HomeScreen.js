7c70f717b19ddcb60322645b49d88ccf5da4c245
Yasir1986 <yasir.akbar86@hotmail.com>
2022-08-29 19:59:37 +0100

    boilerplate for plantShop UI

---
+++ src/view/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HOMESCREEN1</Text>
+        </View>
+    );
+};
+
+
+export default HomeScreen;
